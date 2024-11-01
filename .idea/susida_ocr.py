364bdfabb8396b1ab6ecabe951612a9677f6663a
raindazo <rein62168@gmail.com>
2024-11-01 15:09:22 +0900

    微調整

--- .idea/susida_ocr.py
+++ .idea/susida_ocr.py
@@ -1,5 +1,4 @@
 import os
-
 import pyocr
 import time
 import pyautogui
@@ -9,32 +8,31 @@ from pynput.keyboard import Controller
 # Tesseractのパスを指定
 pyocr.tesseract.TESSERACT_CMD = r'E:/dev/Tesseract-OCR/tesseract.exe'
 
-tools = pyocr.get_available_tools()
-if len(tools) == 0:
-    print("OCRツールを取得できませんでした。")
-    exit(2)
-
-tools = tools[0]
+#ツール取得
+tools = pyocr.get_available_tools()[0]
 keyboard = Controller()
 
 # スクリーンショット保存パス
-screenshot_folader = r'E:\dev\OCR_test'
+screenshot_folder = r'E:\dev\OCR_test'
+# 撮影する範囲指定
+region = (700, 430, 450, 35)
 
 # 試行回数
 attempts = 1000
-# 撮影する範囲指定
-region = (700, 430, 450, 35)
+
 time.sleep(0.5)
 print("処理開始")
+
 try:
     for i in range(attempts):
-        screenshot = os.path.join(screenshot_folader, f'screenshot_{i}.png')
+        screenshot = os.path.join(screenshot_folder, f'{i}.png')
         pyautogui.screenshot(screenshot, region=region)
 
         img = Image.open(screenshot)
         builder = pyocr.builders.TextBuilder(tesseract_layout=6)
         text = tools.image_to_string(img, lang='jpn', builder=builder)
 
+        #OCRで取得した文字をタイプ
         for char in text:
             keyboard.type(char)
 
@@ -44,9 +42,9 @@ except KeyboardInterrupt:
 
 # 終了時スクリーンショットを削除する
 def delete_png_files():
-    for tidying in os.listdir(screenshot_folader):
+    for tidying in os.listdir(screenshot_folder):
         if tidying.endswith('.png'):
-            file_path = os.path.join(screenshot_folader, tidying)
+            file_path = os.path.join(screenshot_folder, tidying)
             os.remove(file_path)
     print("お片付け完了！")
 
